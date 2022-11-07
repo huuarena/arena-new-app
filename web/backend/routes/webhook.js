d358320701f8697c9732271ede9de5082fc136a5
huuarena <huupham.arena@gmail.com>
2022-11-07 16:41:57 +0700

    update webhook

--- web/backend/routes/webhook.js
+++ web/backend/routes/webhook.js
@@ -3,7 +3,5 @@ import Controller from '../controllers/webhook.js'
 export default function webhookRoute(app) {
   app.post('/api/webhooks', Controller.process)
   app.get('/api/webhooks', Controller.get)
-  app.put('/api/webhooks', Controller.get)
-  app.delete('/api/webhooks', Controller.get)
-  app.patch('/api/webhooks', Controller.get)
+  app.get('/api/webhooks/*', Controller.get)
 }
