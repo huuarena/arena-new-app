4b7997e6f8894c9be46e2a06712e70bf5f0f889b
huuarena <huupham.arena@gmail.com>
2022-11-08 11:23:16 +0700

    update response status

--- web/backend/helpers/responseHandler.js
+++ web/backend/helpers/responseHandler.js
@@ -1,9 +1,14 @@
+/**
+ * HTTP response status codes
+ * https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
+ */
+
 const ResponseHandler = {
   success: (res, data) => {
     res.status(200).json({ success: true, data })
   },
   error: (res, error, status) => {
-    res.status(status || 500).json({ success: false, error: { ...error, message: error.message } })
+    res.status(status || 400).json({ success: false, error: { ...error, message: error.message } })
   },
 }
 
