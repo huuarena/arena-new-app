91f022812b63c90bdc4e79d1f0cc44f5ba32312a
phamhuu2701 <phamhuu2701@gmail.com>
2022-11-13 10:30:05 +0700

    update format date time

--- web/frontend/helpers/formatDateTime.js
+++ web/frontend/helpers/formatDateTime.js
@@ -7,44 +7,35 @@ import moment from 'moment'
  * @returns String
  */
 const formatDateTime = (datetime, type = 'YYYY-MM-DD') => {
-  let _datetime = new Date(datetime).getTime()
-
-  const newDate = new Date(_datetime)
-  const yyyy = newDate.getFullYear()
-  const yyyyStr = `${yyyy}`
-  const mm = newDate.getMonth() + 1
-  const mmStr = `${mm < 10 ? `0` : ``}${mm}`
-  const dd = newDate.getDate()
-  const ddStr = `${dd < 10 ? `0` : ``}${dd}`
-
   switch (type) {
-    case 'YYYY/MM/DD':
-      // 2020/02/26
-      return `${yyyyStr}/${mmStr}/${ddStr}`
-
-    case 'YYYY-MM-DD':
-      // 2020-02-26
-      return `${yyyyStr}-${mmStr}-${ddStr}`
-
     case 'MM/DD/YYYY':
-      // 02/26/2020
-      return `${mmStr}/${ddStr}/${yyyyStr}`
+    case 'L':
+      return moment(new Date(datetime)).format('L')
 
     case 'Month DD, YYYY':
-      // December 25, 2020
-      return moment(_datetime).format('LL')
-
     case 'LL':
-      // December 25, 2020
-      return moment(_datetime).format(type)
+      // Month DD, YYYY
+      return moment(new Date(datetime)).format('LL')
 
     case 'LLL':
-      // December 29, 2020 12:00 AM
-      return moment(_datetime).format(type)
+      // Month DD, YYYY HH:MM AM
+      return moment(new Date(datetime)).format(type)
+
+    case 'LLLL':
+      // Day, Month DD, YYYY HH:MM AM
+      return moment(new Date(datetime)).format(type)
 
     default:
-      // 2020-02-26
-      return `${yyyyStr}-${mmStr}-${ddStr}`
+      // YYYY/MM/DD
+      let date = new Date(datetime)
+
+      let yyyy = date.getFullYear()
+      let mm = date.getMonth() + 1
+      mm = (mm < 10 ? '0' : '') + mm
+      let dd = date.getDate()
+      dd = (dd < 10 ? '0' : '') + dd
+
+      return `${yyyy}/${mm}/${dd}`
   }
 }
 
